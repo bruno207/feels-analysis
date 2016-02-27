177a498433e657af261a594bb4d3a04768cee9cc
David Haynes <dhaynes3@gmu.edu>
2016-02-27 02:51:56 -0500

    JSON Gathering from Last.fm + index testing
    - Data gathering part one
    - key redacted but the code works pulling one artist, song from json request
    from last.fm
    - it's 2:53 am and I am barely tired and have no idea of what sleep means, can I make it?

--- feels_analysis/feels_analysis/views.py
+++ feels_analysis/feels_analysis/views.py
@@ -6,5 +6,16 @@ from django.conf import settings
 from django.http import HttpResponseServerError  # Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
+import requests, json
+
 def index(request):
-    return render(request, 'index.html', {},)
+    payload = {
+        "api_key": 'REDACETED',
+        "method": 'chart.getTopTracks',
+        "limit": '1',
+        "format": 'json' }
+    r = requests.get('http://ws.audioscrobbler.com/2.0/', params=payload)
+    return render(request, 'index.html', {
+        'data0': r.json()["tracks"]["track"][0]["name"],
+        'data': r.json()["tracks"]["track"][0]["artist"]["name"]
+    },)
