c1848d1286ea07aafa68730f1ccb2f5cdaddea77
David Haynes <dhaynes3@gmu.edu>
2016-02-27 01:02:21 -0500

    Index page now exists
    - points to a FBV now because CBV was acting up

--- feels_analysis/feels_analysis/urls.py
+++ feels_analysis/feels_analysis/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
-import views
-
+from .views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name=index),
+    url(r'^$', index, name='index'),
 ]
