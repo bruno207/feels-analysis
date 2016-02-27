e4ccc77cc4d055bd70b979574bdc2c67f3e06df9
David Haynes <dhaynes3@gmu.edu>
2016-02-27 09:37:12 -0500

    Added a model to input Tracks into the database
    - basic attributes
    - it werks

--- feels_analysis/feels_analysis/models.py
+++ feels_analysis/feels_analysis/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+
+class Track(TimeStampedModel):
+    track_name = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+
+    track_id = models.IntegerField(default=0)
+    lyrics = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.track_name
+
+    def __unicode__(self):
+        return unicode(self.track_name)
